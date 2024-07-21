b585da957fc0d0dbcbcaff358716fe716fd75e26
chinchillajared <chinchillajared@gmail.com>
2024-07-20 19:02:06 -0600

    Clearing messages to avoid message overlapping

--- scripts/connect.js
+++ scripts/connect.js
@@ -11,12 +11,14 @@ socket.onopen = function (event) {
 
 socket.onmessage = function (event) {
     console.log('Received from server: ' + event.data);
+    document.getElementById('messages').innerText = ''; // clear the messages
     document.getElementById('messages').innerText += 'Received from server: ' + event.data + '\n';
 };
 
 
 socket.onclose = function (event) {
     console.log('WebSocket is closed now.');
+    document.getElementById('messages').innerText = ''; // clear the messages
     document.getElementById('status').innerText += ' Not connected.';
 };
 
